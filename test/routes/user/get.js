6b541805ffbc5b2732350541402c35735342aed6
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-02 13:32:55 -0600

    to caleb

---
+++ test/routes/user/get.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { expect } = require('chai');
+const request = require('supertest');
+
+const app = require('../../../app');
+
+describe('routes', () => {
+  describe('/user/:id', () => {
+    let server;
+
+    beforeEach( (done) => {
+      server = app.listen(process.env.PORT || 7357, (err) => {
+        console.log(`listening on ${server.address().port}`);
+      })
+    })
+
+    afterEach( (done) => {
+      server.close(done);
+    })
+    it('returns a 400 if no :id is included', (done) => {
+      request('http://localhost:7357')
+      .get('/user')
+      .expect(400)
+      .end(done)
+    })
+  })
+})
